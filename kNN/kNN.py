9f28f179ab4f84c62e9f01dc97d4375ae82da0a2
yaominguo <missgmy@yahoo.com>
2017-11-23 16:39:17 +0800

    手写数字识别系统测试

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,6 +1,15 @@
 #encoding:utf-8
 from numpy import *
+from os import listdir
 import operator
+
+
+###k-近邻算法是分类数据最简单最有效的算法，使用该算法时必须有接近实际数据的训练样本数据。
+###k-近邻算法必须保存全部数据集，如果训练数据集很大，必须使用大量的存储空间。
+###此外，由于必须对数据集中的每个数据计算距离值，实际使用时可能非常耗时。
+###k-近邻算法的另一个缺陷是它无法给出任何数据的基础结构信息，因此我们也无法得知平均实例样本和典型实例样本具有什么特征。
+
+
 
 
 def createDataSet():
@@ -63,7 +72,7 @@ def autoNorm(dataSet):
 ##测试错误率
 def datingClassTest():
     hoRatio=0.10
-    datingDataMat,datingLabels=file2matrix('datingTestSet.txt')##读取文件数据
+    datingDataMat,datingLabels=file2matrix('src/datingTestSet.txt')##读取文件数据
     normMat,ranges,minVals=autoNorm(datingDataMat) ##转换为归一化特征值
     m=normMat.shape[0]
     numTestVecs=int(m*hoRatio)
@@ -77,21 +86,55 @@ def datingClassTest():
     print "the total error rate is: %f" % (errorCount/float(numTestVecs)) ##返回错误率为0.050000,约为5%
 
 
-##预测函数
+##预测喜欢与否的函数
 def classifyPerson():
     resultList=['not at all', 'in small doses', 'in large doses']
     percentTats=float(raw_input("Percentage of time spent playing video games?"))
     ffMiles=float(raw_input("Frequent flier miles earned per year?"))
     iceCream=float(raw_input("Liters of ice cream consumed per year?"))
-    datingDataMat,datingLabels=file2matrix('datingTestSet.txt')
+    datingDataMat,datingLabels=file2matrix('src/datingTestSet.txt')
     normMat,ranges,minVals = autoNorm(datingDataMat)
     inArr=array([ffMiles,percentTats,iceCream])
     classifierResult=classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
     print "You will probable like this person: ",resultList[classifierResult-1]
 
+##将图像转换为向量
+def img2vector(filename):
+    returnVect=zeros((1,1024)) #创建1*1024的NumPy数组
+    fr=open(filename)
+    for i in range(32): #循环读取文件的前32行
+        lineStr=fr.readline()
+        for j in range(32): #每行的头32个字符值存储在数组中
+            returnVect[0,32*i+j]=int(lineStr[j])
+    return returnVect   #返回该数组
 
+##手写数字识别系统的测试代码
+def handwritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('src/trainingDigits') #获取目录内容
+    m=len(trainingFileList) #获取文件数量存储在m中
+    trainingMat=zeros((m,1024))  #穿件一个m行1024列的训练矩阵，该矩阵每行数据存储一个图像
+    for i in range(m):  #从文件名解析分类数字
+        fileNameStr=trainingFileList[i]
+        fileStr=fileNameStr.split('.')[0] #截取.txt之前的文件名
+        classNumStr=int(fileStr.split('_')[0]) #截取 _ 之前的数字
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('src/trainingDigits/%s' %fileNameStr)
 
-
+    testFileList=listdir('src/testDigits')
+    errorCount=0.0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        vectorUnderTest=img2vector('src/testDigits/%s' %fileNameStr)
+        classifierResult=classify0(vectorUnderTest,trainingMat,hwLabels,3) #使用classify0函数测试该目录下的每个文件
+        print "The classifier came back with: %d, the real answer is: %d" %(classifierResult,classNumStr)
+        if(classifierResult!=classNumStr):
+            errorCount+=1.0
+    print "\nThe total number of errors is: %d" %errorCount
+    print "\nThe total error rate is: %f" %(errorCount/float(mTest))
 
 
 
