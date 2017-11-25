accb4cc1868ee8e72e9e02ff32be39435cb53fcb
yaominguo <missgmy@yahoo.com>
2017-11-25 16:41:48 +0800

    matplotlib注解绘制树形图

--- DecisionTrees/treePlotter.py
+++ DecisionTrees/treePlotter.py
@@ -1,18 +1,85 @@
-#coding=utf-8
+# coding=utf-8
 import matplotlib.pyplot as plt
 
+##定义文本框和箭头格式
+decisionNode = dict(boxstyle='sawtooth', fc='0.8')
+leafNode = dict(boxstyle='round4', fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+
+
+##绘制带箭头的注释
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt, textcoords='axes fraction',
+                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+##在父子节点间填充文本信息
+def plotMidText(cntrPt,parentPt,txtString):
+    xMid=(parentPt[0]-cntrPt[0])/2.0+cntrPt[0]
+    yMid=(parentPt[1]-cntrPt[1])/2.0+cntrPt[1]
+    createPlot.ax1.text(xMid,yMid,txtString)
+
+def plotTree(myTree,parentPt,nodeTxt):
+    numLeafs=getNumLeafs(myTree)
+    depth=getTreeDepth(myTree)
+    firstStr=myTree.keys()[0]
+    cntrPt=(plotTree.xOff+(1.0+float(numLeafs))/2.0/plotTree.totalW,plotTree.yOff)
+    plotMidText(cntrPt,parentPt,nodeTxt) #绘出子节点具有的特征值，或者沿此分支向下的数据实例必须具有的特征值
+    plotNode(firstStr,cntrPt,parentPt,decisionNode)
+    secondDict=myTree[firstStr]
+    plotTree.yOff=plotTree.yOff-1.0/plotTree.totalD #按比例减少全局变量yOff
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xOff=plotTree.xOff+1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+            plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+    plotTree.yOff=plotTree.yOff+1.0/plotTree.totalD
+
+
 ##使用文本注解绘制树节点
-decisionNode=dict(boxstyle='sawtooth',fc='0.8')
-leafNode=dict(boxstyle='round4',fc='0.8')
-arrow_args=dict(arrowstyle='<-')
-
-def plotNode(nodeTxt,centerPt,parentPt,nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt,xycoords='axes fraction',xytext=centerPt,textcoords='axes fraction',va='center',ha='center',bbox=nodeType,arrowprops=arrow_args)
-
-
-def createPlot():
-    fig=plt.figure(1,facecolor='white')
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
     fig.clf()
-    createPlot.ax1=plt.subplot(111,frameon=False)
-    plotNode('a decision node',(0.5,0.1),(0.1,0.5),decisionNode)
-    plotNode('a leaf node',(0.8,0.1),(0.3,0.8),leafNode)
+    axprops=dict(xticks=[],yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False,**axprops)
+    plotTree.totalW=float(getNumLeafs(inTree))
+    plotTree.totalD=float(getTreeDepth(inTree))
+    plotTree.xOff=-0.5/plotTree.totalW;plotTree.yOff=1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
+
+
+##获取叶节点的数目
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 判断子节点是否为字典类型
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+##获取树的层数
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 判断子节点是否为字典类型
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+                   ]
+    return listOfTrees[i]
